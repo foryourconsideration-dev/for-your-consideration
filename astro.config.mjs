eab4bc1b8acfec453a05b7ec60143393b6ac12b7
foryourconsideration-dev <317757958+foryourconsideration-dev@users.noreply.github.com>
2026-08-20 15:28:02 -0700

    feat: add sitemap and crawler discovery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  integrations: [
+    sitemap({
+      filter: (page) => {
+        const { pathname } = new globalThis.URL(page);
+
+        return (
+          pathname !== "/404/" &&
+          pathname !== "/robots.txt" &&
+          !pathname.startsWith("/preview/")
+        );
+      },
+    }),
+  ],
   site: "https://foryourconsideration.blog",
 });
